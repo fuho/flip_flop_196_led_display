bca937280801dad6a0c56b6463efd4a90bf799c2
fuho <ondrej.dolejsi@gmail.com>
2013-04-29 09:45:04 -0400

    Both segment and pixel screen draw works. Seeminglk no difference in brightness. Pixel redraw should fix the small issue with lower colours looking brighter when only using one or couple outputs of flip-flops

--- src/sketch.ino
+++ src/sketch.ino
@@ -2,7 +2,7 @@
 #define DEBUG false
 #define VIDEO_RAM_LENGTH 196
 #define VERTICAL_SEGMENT_ID 4
-#define DISPLAY_SHADES 14
+#define DISPLAY_SHADES 7
 #define DISPLAY_ROWS 7
 #define DISPLAY_COLUMN_SEGMENTS 4
 #define DISPLAY_SEGMENT_WIDTH 7
@@ -12,59 +12,78 @@
 // Global variable declarations ###############################################
 byte video_ram[196];                // Display image data storage
 int iter_shade      = 0,            // Colour(shade) counter,
+    iter_seg_pos    = 0,            // Vertical column counter (draw_pixel)
     iter_row        = 0,            // Horizontal line counter
-    iter_segment    = 0;            // Vertical segment row counter
+    iter_segment    = 0;            // Vertical segment row counter (draw_segment)
+byte data_segment  = 0;            // Stores segment pixel data
 boolean SLOW_DOWN = false;
  
 // Setup ######################################################################
 void setup() {
+    // Set ports for outputs
     DDRD = DDRD | B11111100;
     DDRB = DDRB | B00111111;
+
     reset_screen();
+
+    gradient_fill();
+    //white_fill();
+    //test_fill();
+    
+    prep_segment_data();
+
     // Initialize input delay selector
     pinMode(A1,INPUT);
     digitalWrite(A1, HIGH); //init pullup
     if(digitalRead(A1)){
         SLOW_DOWN = true;
     }
+
     // Initialize serial port
     Serial.begin(115200);
 
-    gradient_fill();
-    //white_fill();
-    //test_fill();
-    
     video_ram_out();
 }
 
 // Main Program Loop ##########################################################
 void loop() {
-    reset_screen();
-    draw_segment();
+    //reset_screen();
+    //draw_segment();
+    //iterate_segment();
+
+
+    draw_pixel();
+    iterate_pixel();
+
     //if(SLOW_DOWN) delay(200);
+    //delayMicroseconds(10);
     
-    iterate();
 }
 // Function definitions #######################################################
 
 // ############################################################################
 // horizontal (left to right top to bottom) drawing procedure
 void reset_screen(){
+    // all horizontal flipflops to 11111111
     PORTB = 1;      // set one last pixel to none
     PORTD = 252;    // set LSB to none
     PORTB = 31;     // burn it to 4 horozontal flip-flops
 
-    pass;
+    pass;           // Make sure compiler doesn't optimize 31 and THEN 0 to 0
+
+    // vertical flip-flop to 0000000
     PORTB = 0;
     PORTD = 0;
     PORTB = 32;
 }
+// ========== DRAW BY SEGMENT =================================================
 void draw_segment(){
     //            x-tal          Rx/Tx
     //             ↓                            ↓
     //             ??VDCBA6 543210??    pixel data: 100 0000 row: 1 segment:2
     //                                  Load Pixel Data (Horizontal negative)
-    byte pixel_data = (~get_segment_data()) << 1;
+    prep_segment_data();
+    byte pixel_data = (~data_segment) << 1;
     PORTB = pixel_data >> 7;
     PORTD = pixel_data << 1;
     PORTB = PINB | (2 << iter_segment);
@@ -73,8 +92,8 @@ void draw_segment(){
     PORTD = 4 << iter_row;
     PORTB = PINB | 32;
 }
-byte get_segment_data(){
-    // returns a byte representing a segment at current position and time
+void prep_segment_data(){
+    // prepares a byte representing a segment at current position and time
     // time is represented by shade iterator
     //We have iter_row, iter_segment and iter_shade
     byte data;
@@ -86,9 +105,9 @@ byte get_segment_data(){
             data = data | 1;
         }
     }
-    return data;
+    data_segment =  data;
 }
-void iterate(){
+void iterate_segment(){
     if(++iter_segment > DISPLAY_COLUMN_SEGMENTS - 1){
         iter_segment = 0;
         iter_row++;
@@ -102,9 +121,45 @@ void iterate(){
     }
 }
 
+// ========== DRAW BY PIXEL ===================================================
+void draw_pixel(){
+    //            x-tal          Rx/Tx
+    //             ↓                            ↓
+    //             ??VDCBA6 543210??    pixel data: 100 0000 row: 1 segment:2
+    //                                  Load Pixel Data (Horizontal negative)
+    byte pixel_data = (~(data_segment & (1 << iter_seg_pos) )) << 1;
+    PORTB = pixel_data >> 7;
+    PORTD = pixel_data << 1;
+    PORTB = PINB | (2 << iter_segment);
+    
+    PORTB = (2 << iter_row) >> 7;
+    PORTD = 4 << iter_row;
+    PORTB = PINB | 32;
+}
+void iterate_pixel(){
+    if(++iter_seg_pos > DISPLAY_SEGMENT_WIDTH - 1){
+        iter_seg_pos = 0;
+        iter_segment++;
+    }
+    if(iter_segment > DISPLAY_COLUMN_SEGMENTS - 1){
+        iter_segment = 0;
+        iter_row++;
+    }
+    if(iter_row > DISPLAY_ROWS - 1 ){
+        iter_row = 0;
+        iter_shade++;
+    }
+    if(iter_shade > DISPLAY_SHADES -1){
+        iter_shade = 0;
+    }
+    if(0 ==iter_seg_pos){
+        prep_segment_data();
+        reset_screen();
+    }
+}
+
 // ############################################################################
 // other functions ############################################################
-
 void gradient_fill(){
     byte color = DISPLAY_SHADES;
     for (int i = 0; i < VIDEO_RAM_LENGTH; i++){
@@ -115,13 +170,11 @@ void gradient_fill(){
         }
     }
 }
-
 void white_fill(){
     for (int i = 0; i<196; i++){
         video_ram[i] = byte(DISPLAY_SHADES);
     }
 }
-
 void test_fill(){
     video_ram[0] = 7;
     video_ram[1] = 6;
@@ -178,7 +231,6 @@ void test_fill(){
     video_ram[180 + 7] = 0;
 
 }
-
 void video_ram_out(){
     for (int i =0; i < VIDEO_RAM_LENGTH; i++ ){
         if(!( i % 28)){
@@ -188,7 +240,6 @@ void video_ram_out(){
     }
     Serial.println();
 }
-
 void dbg(char text[], word value){
     if(DEBUG){
         Serial.print(iter_shade);
@@ -201,7 +252,9 @@ void dbg(char text[], word value){
         Serial.println(value, BIN);
     }
 }
-
+/*
+ * Help to understand the problem
+ 
 void _loop() {
     //            x-tal          Rx/Tx
     //             ↓                            ↓
@@ -236,3 +289,4 @@ void _loop() {
     
     delay(200);
 }
+*/
